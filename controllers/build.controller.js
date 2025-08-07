158fe646bee0d7e68f91cc84e008008c5ab7a6d6
carvesco <carvesco@gmail.com>
2025-08-07 10:41:11 -0400

    updated create session

--- controllers/build.controller.js
+++ controllers/build.controller.js
@@ -2,24 +2,15 @@ import Build from "../models/build.model.js";
 import mongoose from "mongoose";
 import { validateLoginId } from "./user.controller.js";
 
-export const getBuild = async (req, res) => {
+export const getBuild = async (buildNumber) => {
   try {
-    let id = req.query.id;
-    let loginId = req.query.loginId;
-    let activeSession = await validateLoginId(loginId);
-    if (!activeSession) {
-      return res.status(403).json({ msg: "User is in an active session" });
-    }
-    if (isNaN(id) || !Number.isInteger(Number(id))) {
-      return res.status(422).json({ msg: `${id} is not a valid id` });
-    }
-    const build = await Build.findOne({ BuildNumber: id });
+    console.log("Fetching build for buildNumber:", buildNumber);
+    const build = await Build.findOne({ buildNumber });
     if (!build) {
-      return res.status(404).json({ msg: `No build with number :${id}` });
+      return null;
     }
-
-    res.status(200).json(build);
+    return build;
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    throw new Error(`Error fetching build: ${error.message}`);
   }
 };
--- controllers/session.controller.js
+++ controllers/session.controller.js
@@ -1,10 +1,33 @@
+import Build from "../models/build.model.js";
 import Session from "../models/session.model.js";
+import { getBuild } from "./build.controller.js";
 import { endSession } from "./user.controller.js";
 
 export const createSession = async (req, res) => {
   try {
+    const { loginId, buildNumber } = req.body;
+    if (!loginId || !buildNumber) {
+      return res
+        .status(400)
+        .json({ msg: "loginId and buildNumber are required" });
+    }
+    let buildData = await getBuild(buildNumber);
+    console.log("Build Data:", buildData);
+    if (!buildData) {
+      return res.status(404).json({ msg: "Build not found" });
+    }
+    const sessionExists = await Session.findOne({ loginId, buildNumber });
+    if (sessionExists) {
+      return res.status(200).json({
+        msg: "Session already exists for this user and build",
+        buildData,
+      });
+    }
     await Session.create(req.body);
-    res.status(201).json({ msg: "Session created successfully" });
+    return res.status(200).json({
+      msg: "Session created successfully",
+      buildData,
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
